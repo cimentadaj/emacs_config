31c6fe77c2fcdd2b52694b8034f86276023b9b2c
cimentadaj <cimentadaj@gmail.com>
2019-05-26 11:30:05 +0200

    Adds ESS stuff to EMACS

--- init.el
+++ init.el
@@ -117,4 +117,65 @@
   :config (load-theme 'spacemacs-dark t))
 
 
+;; Load ESS
+
+(use-package ess
+  :ensure t
+  :init (require 'ess-site))
+
+;; Make Shift-Enter do a lot in ESS
+
+(setq ess-ask-for-ess-directory nil)
+(add-hook 'inferior-ess-mode-hook
+    '(lambda nil
+          (define-key inferior-ess-mode-map [\C-up]
+              'comint-previous-matching-input-from-input)
+          (define-key inferior-ess-mode-map [\C-down]
+              'comint-next-matching-input-from-input)
+          (define-key inferior-ess-mode-map [\C-x \t]
+              'comint-dynamic-complete-filename)
+     )
+ )
+
+(setq ess-ask-for-ess-directory nil)
+  (setq ess-local-process-name "R")
+  (setq ansi-color-for-comint-mode 'filter)
+  (setq comint-scroll-to-bottom-on-input t)
+  (setq comint-scroll-to-bottom-on-output t)
+  (setq comint-move-point-for-output t)
+
+  (defun my-ess-start-R ()
+    (interactive)
+    (if (not (member "*R*" (mapcar (function buffer-name) (buffer-list))))
+      (progn
+        (delete-other-windows)
+        (setq w1 (selected-window))
+        (setq w1name (buffer-name))
+        (setq w2 (split-window w1 nil t))
+        (R)
+        (set-window-buffer w2 "*R*")
+        (set-window-buffer w1 w1name))))
+
+  (defun my-ess-eval ()
+    (interactive)
+    (my-ess-start-R)
+    (if (and transient-mark-mode mark-active)
+        (call-interactively 'ess-eval-region)
+      (call-interactively 'ess-eval-line-and-step)))
+;; I was trying to add that whenever it finds a paragraph it executes the complete
+;; parapgraph or a function. However, I couldn't get it to work and now I use
+;; C-c C-c which executes it as I want it.
+;;      (call-interactively 'ess-eval-region-or-function-or-paragraph-and-step)))
+
+  (Add-hook 'ess-mode-hook
+            '(lambda()
+               (local-set-key [(shift return)] 'my-ess-eval)))
+  (add-hook 'inferior-ess-mode-hook
+            '(lambda()
+               (local-set-key [C-up] 'comint-previous-input)
+               (local-set-key [C-down] 'comint-next-input)))
+ (add-hook 'Rnw-mode-hook
+          '(lambda()
+             (local-set-key [(shift return)] 'my-ess-eval)))
+ (require 'ess-site)
 
