03ec2cd42ae8a153cc022d7c8dc8a244cbd31395
Jorge Cimentada <cimentadaj@gmail.com>
2019-06-10 10:28:02 +0200

    Removes all other folders except myinit

--- elpa/ewal-20190518.533/ewal-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ewal" "20190518.533" "A pywal-based theme generator" '((emacs "25")) :commit "7f65b109e597379045d044e324e7bb64caa65344" :keywords '("faces") :authors '(("Uros Perisic")) :maintainer '("Uros Perisic") :url "https://gitlab.com/jjzmajic/ewal")
